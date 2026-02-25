2f45bd48f81ae54b7c4c04347dcbb0c4e5f252b1
Itoro <itoroabasi.unang@dpdk.com>
2026-02-25 21:35:36 +0100

    main

--- app.config.js
+++ app.config.js
@@ -1,4 +1,8 @@
-{
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default {
   "expo": {
     "name": "Laundrify DryCleaners",
     "slug": "laundrify-drycleaners",
@@ -60,14 +64,14 @@
         "projectId": "b6aa89ad-7078-42cc-9a09-e4e0437dcdc0"
       },
       "firebaseConfig": {
-        "apiKey": "AIzaSyAAzR567g2PQGRFYVt8bBZ5MJWOZ3FVSHQ",
-        "authDomain": "laundrify-ad7d9.firebaseapp.com",
-        "projectId": "laundrify-ad7d9",
-        "storageBucket": "laundrify-ad7d9.firebasestorage.app",
-        "messagingSenderId": "794067729526",
-        "appId": "1:794067729526:web:9e5c852aa09d21eb1bf357",
-        "measurementId": "G-595G56VEBL"
+        "apiKey": process.env.FIREBASE_API_KEY,
+        "authDomain": process.env.FIREBASE_AUTH_DOMAIN,
+        "projectId": process.env.FIREBASE_PROJECT_ID,
+        "storageBucket": process.env.FIREBASE_STORAGE_BUCKET,
+        "messagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID,
+        "appId": process.env.FIREBASE_APP_ID,
+        "measurementId": process.env.FIREBASE_MEASUREMENT_ID
       }
     }
   }
-}
+};
