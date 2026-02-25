5c82a85e6e7ef7e9554520f89e5d445eb5c43355
Itoro <itoroabasi.unang@dpdk.com>
2026-02-25 22:35:14 +0100

    main

--- supabase.js
+++ supabase.js
@@ -1,12 +1,10 @@
 // supabase.js
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const supabaseUrl = 'https://abbyesfjcvovdbrpbmuc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFiYnllc2ZqY3ZvdmRicnBibXVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NDM5OTQsImV4cCI6MjA4NDMxOTk5NH0.l4rADjNSOpR0lg0DDGZD895Ot9w2eZ2d_oQ7U4wtmyg';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 
 // Regular client 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
